4bfb2915fe817c145389da6c492d009fbea98697
Eva <eva@allwin.bid>
2015-10-16 19:00:10 +0900

    vertical seek bar demo

--- app/src/main/java/com/stewhouse/updownseekbar/UDMainActivity.java
+++ app/src/main/java/com/stewhouse/updownseekbar/UDMainActivity.java
@@ -2,32 +2,60 @@ package com.stewhouse.updownseekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 
 public class UDMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        SeekBar defaultSeekBar;
+
+        defaultSeekBar = (SeekBar) findViewById(R.id.defalutSeekBar);
+        if (defaultSeekBar != null) {
+            defaultSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                    // Get progress.
+                    Log.e("onProgressChanged", "PROGRESS: " + progress);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    // 선택 막대를 터치하고 드래그를 시작할 때 실행되는 메서드.
+                    Log.e("onStartTrackingTouch", "DRAGGING");
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    // 선택을 완료했을 때 표시하는 메서드.
+                    Log.e("onStopTrackingTouch", "DRAGGING COMPLETE");
+                }
+            });
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
---
+++ app/src/main/java/com/stewhouse/updownseekbar/UpDownSeekBar.java
@@ -0,0 +1,64 @@
+package com.stewhouse.updownseekbar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.SeekBar;
+
+/**
+ * Created by Allwin-Eva on 15. 10. 16..
+ */
+
+// REFERENCE: https://github.com/AndroSelva/Vertical-SeekBar-Android
+
+public class UpDownSeekBar extends SeekBar {
+
+    public UpDownSeekBar(Context context) {
+        super(context);
+    }
+
+    public UpDownSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UpDownSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    protected void onDraw(Canvas c) {
+        c.rotate(-90);
+        c.translate(-getHeight(), 0);
+
+        super.onDraw(c);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                int i = 0;
+                i = getMax() - (int) (getMax() * event.getY() / getHeight());
+                setProgress(i);
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
+    }
+}
